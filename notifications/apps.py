45971319fd24b53fb2ad1a041b413997df988bc5
Hitesh <hiteshna@atrihub.io>
2025-11-11 14:07:21 -0800

    refactor: Rename qa_runs app to notifications and update related configurations

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QaRunsConfig(AppConfig):
+class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'qa_runs'
+    name = 'notifications'
--- studybud/settings.py
+++ studybud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,6 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
 
     'rest_framework',
     "corsheaders",
