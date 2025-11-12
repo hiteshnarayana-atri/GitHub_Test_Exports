a6d3a65512691de69f4c9a50d50a10f2b06c8201
Hitesh <hiteshna@atrihub.io>
2025-11-12 09:19:32 -0800

    feat: Integrate Allure reporting for Cypress tests and update dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require('cypress')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://127.0.0.1:8000/',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    setupNodeEvents(on, config) {
+      allureWriter(on, config)
+      return config
+    },
   },
   reporter: 'mocha-junit-reporter',
   reporterOptions: {
